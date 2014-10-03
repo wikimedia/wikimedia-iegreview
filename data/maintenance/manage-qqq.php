6b1ec5f10eddd763f427db233626daaaab1fc37a
Bryan Davis <bd808@wikimedia.org>
2014-10-02 20:34:29 -0600

    Fixup messages
    Remove unused messages and ensure that all messages have qqq
    descriptions.

--- data/maintenance/manage-qqq.php
+++ data/maintenance/manage-qqq.php
@@ -3,8 +3,8 @@
  * Ensure that the qqq file has all keys in en.json and no others.
  */
 
-$en_file = __DIRNAME__ . '/../i18n/en.json';
-$qqq_file = __DIRNAME__ . '/../i18n/qqq.json';
+$en_file = __DIR__ . '/../i18n/en.json';
+$qqq_file = __DIR__ . '/../i18n/qqq.json';
 
 $en = json_decode( file_get_contents( $en_file ), true );
 $qqq = json_decode( file_get_contents( $qqq_file ), true );
@@ -21,5 +21,9 @@ foreach ( $qqq as $key => $value ) {
 	}
 }
 
-//ksort( $qqq );
+$meta = $qqq['@metadata'];
+unset( $qqq['@metadata'] );
+ksort( $qqq );
+
+$qqq = array_merge( array( '@metadata' => $meta ), $qqq );
 file_put_contents( $qqq_file, json_encode( $qqq, JSON_PRETTY_PRINT ) );
