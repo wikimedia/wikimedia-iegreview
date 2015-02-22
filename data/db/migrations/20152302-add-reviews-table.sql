488031b064603421baaec24dcd3bbc82d003ab76
Niharika <niharikakohli29@gmail.com>
2015-02-22 18:28:17 +0000

    Add reviews and review_questions tables
    Renamed the old reviews table to 'reviews_legacy' and new one is now 'reviews'
    
    Bug: T89825
    Change-Id: I0e002a43758101c17ecdb208c66c816c3cc7a247

---
+++ data/db/migrations/20152302-add-reviews-table.sql
@@ -0,0 +1,29 @@
+-- Create a new reviews and review_questions table to make them customizable
+
+RENAME TABLE reviews to reviews_legacy;
+
+
+DROP TABLE IF EXISTS review_questions;
+CREATE TABLE IF NOT EXISTS review_questions (
+    id              INT(11) NOT NULL
+  , campaign        INT(11) NOT NULL
+  , question        VARCHAR(255)
+  , added_by        INT(11)
+  , created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  , PRIMARY KEY (id)
+  , CONSTRAINT FOREIGN KEY (campaign) REFERENCES campaigns(id) ON DELETE CASCADE
+)  ENGINE=InnoDB DEFAULT CHARACTER SET=utf8 ;
+
+
+CREATE TABLE reviews (
+  , proposal        INT(11) NOT NULL
+  , question        INT(11)
+  , points          INT(11)
+  , comments        VARCHAR(255)
+  , reviewer        INT(11)
+  , created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  , PRIMARY KEY (proposal, question, reviewer)
+  , CONSTRAINT FOREIGN KEY (proposal) REFERENCES proposals (id) ON DELETE CASCADE
+  , CONSTRAINT FOREIGN KEY (question) REFERENCES review_questions(id) ON DELETE CASCADE
+  , CONSTRAINT FOREIGN KEY (reviewer) REFERENCES users(id) ON DELETE RESTRICT
+)  ENGINE=InnoDB DEFAULT CHARACTER SET=utf8 ;
