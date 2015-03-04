21fb936fb0b72874e0ee70d99588cf8fb93844a8
Niharika <niharikakohli29@gmail.com>
2015-03-04 17:49:58 +0000

    Associate campaigns to questions
    Change-Id: I99a7771d753b74ab92ae2546bd8115406c24792a

--- src/Dao/Campaigns.php
+++ src/Dao/Campaigns.php
@@ -120,7 +120,6 @@ class Campaigns extends AbstractDao {
 			')'
 		);
 		return $this->insert( $sql, $data );
-
 	}
 
 
@@ -234,6 +233,79 @@ class Campaigns extends AbstractDao {
 			) );
 			return false;
 		}
+
+	}
+
+
+	/**
+	 * Fetches all questions associated with a given campaign
+	 * @param integer $campaign ID of campaign whose questions are to be fetched
+	 */
+	public function getQuestions( $campaign ) {
+		return $this->fetchAll(
+			self::concat(
+				'SELECT id, question',
+				'FROM review_questions',
+				'WHERE campaign = ?',
+				'ORDER BY id'
+			),
+			array( $campaign )
+		);
+	}
+
+
+	/**
+	 * Inserts new questions into the campaign_questions table
+	 * @param integer $campaign Campaign id
+	 * @param array $questions Array of questions to be added
+	 */
+	public function insertQuestions( $campaign, array $questions ) {
+		$created_by = $this->userId ? : null;
+		$cols = array( 'campaign', 'question', 'created_by' );
+		$params = array_map( function ( $elm ) { return ":{$elm}"; }, $cols );
+		foreach ( $questions as $id => $ques ) {
+			$sql = self::concat(
+				'INSERT INTO review_questions (',
+				implode( ', ', $cols ),
+				') VALUES (',
+				implode( ', ', $params ),
+				')'
+			);
+			$data = array(
+				'campaign' => $campaign,
+				'question' => $ques,
+				'created_by' => $created_by
+			);
+			$this->insert( $sql, $data );
+		}
+		return true;
+	}
+
+
+	/**
+	 * Update questions associated with a campaign
+	 * @param integer $campaign Campaign ID
+	 * @param array $questions Associative array of id=>question(s) to be updated
+	 */
+	public function updateQuestions( $campaign, array $questions ) {
+		$modified_by = $this->userId ? : null;
+		$sql = self::concat(
+			'UPDATE review_questions',
+			'SET question = :question,',
+			'modified_at = :modified_at,',
+			'modified_by = :modified_by',
+			'WHERE id = :id'
+		);
+		foreach( $questions as $id => $ques ) {
+			$data = array(
+				'id' => $id,
+				'question' => $ques,
+				'modified_by' => $modified_by,
+				'modified_at' => date( 'Y-m-d H:i:s' )
+			);
+			$this->update( $sql, $data );
+		}
+
 	}
 
 
