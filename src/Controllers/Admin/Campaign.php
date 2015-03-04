5b8a6fe9b4c039f00c746a5f2fa481991a8a6cda
Niharika <niharikakohli29@gmail.com>
2015-03-04 17:49:58 +0000

    [WIP] Associate campaigns to questions
    Change-Id: I99a7771d753b74ab92ae2546bd8115406c24792a

--- src/Controllers/Admin/Campaign.php
+++ src/Controllers/Admin/Campaign.php
@@ -42,6 +42,7 @@ class Campaign extends Controller {
 				'start_date' => date( 'Y-m-d H:i:s' ),
 				'end_date' => date( 'Y-m-d H:i:s', strtotime( '+30 days' ) ),
 			);
+			$questions = array_fill(1, 4, '');
 		} else {
 			$campaign = $this->dao->getCampaign( $id );
 			$currentReviewers = $this->dao->getReviewers( $id );
@@ -53,10 +54,12 @@ class Campaign extends Controller {
 					}
 				}
 			}
+			$questions = $this->dao->getQuestions( $id );
 		}
 		$this->view->set( 'id', $id );
 		$this->view->set( 'campaign', $campaign );
 		$this->view->set( 'rev', $reviewers );
+		$this->view->set( 'ques', $questions );
 		$this->render( 'admin/campaign.html' );
 	}
 
@@ -72,6 +75,7 @@ class Campaign extends Controller {
 		// Create expectArray method in Form.php
 		// Filed as T90387
 		$this->form->expectAnything( 'reviewer' );
+		$this->form->expectAnything( 'questions' );
 
 		if ( $this->form->validate() ) {
 			$params = array(
@@ -79,6 +83,7 @@ class Campaign extends Controller {
 				'start_date' => $this->form->get( 'start_date' ),
 				'end_date' => $this->form->get( 'end_date' ),
 			);
+			$questions = $this->request->post( 'questions' );
 
 			if ( $id == 'new' && $this->dao->activeCampaign() ) {
 				$this->flash( 'error',
@@ -90,6 +95,7 @@ class Campaign extends Controller {
 				// to be fixed in a subsequent patch when actual logic for using
 				// start and end dates is implemented
 				$params['status'] = 1;
+
 				$newCampaign = $this->dao->addCampaign( $params );
 
 				if ( $newCampaign !== false ) {
@@ -101,6 +107,9 @@ class Campaign extends Controller {
 					$reviewers = $this->request->post( 'reviewer' );
 					if ( $reviewers ) {
 						$this->dao->addReviewers( $id, $reviewers );
+					}
+					if ( $questions ) {
+						$this->dao->insertQuestions( $id, $questions );
 					}
 				} else {
 					$this->flash( 'error',
@@ -121,6 +130,9 @@ class Campaign extends Controller {
 				$diff = Arrays::difference( $oldReviewers, $newReviewers );
 				$this->dao->updateReviewers( $id, $diff );
 
+					if ( $this->dao->updateQuestions( $id, $questions ) ) {
+					}
+
 				if ( $this->dao->updateCampaign( $params, $id ) ) {
 					$this->flash( 'info',
 					$this->i18nContext->message('admin-campaign-update-success' )
@@ -133,9 +145,9 @@ class Campaign extends Controller {
 			}
 
 		$this->redirect( $this->urlFor( 'admin_campaign', array( 'id' => $id ) ) );
-	}
+		}
 
-}
+	}
 
 }
 
