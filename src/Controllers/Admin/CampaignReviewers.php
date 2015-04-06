200369948955f00834188d68dbd285e925cacb04
Niharika <niharikakohli29@gmail.com>
2015-04-06 18:41:28 +0000

    [WIP] Add a report by reviewer for a campaign
    Bug: T87099
    Change-Id: Ib0fbd3c4d5c64af512efb647c85fc8518bb54398

---
+++ src/Controllers/Admin/CampaignReviewers.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @section LICENSE
+ * This file is part of Wikimedia Grants Review application.
+ *
+ * Wikimedia Grants Review application is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Wikimedia Grants Review application is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Wikimedia Grants Review application.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @file
+ * @copyright © 2015 Niharika Kohli, Wikimedia Foundation and contributors.
+ */
+
+namespace Wikimedia\IEGReview\Controllers\Admin;
+
+use Wikimedia\IEGReview\Controller;
+
+/**
+ * Add a new campaign.
+ *
+ * @author Niharika Kohli <nkohli@wikimedia.org>
+ * @copyright © 2014 Niharika Kohli, Wikimedia Foundation and contributors.
+ */
+class CampaignReviewers extends Controller {
+
+	protected function handleGet( $id ) {
+		$records = $this->dao->getReviewerStats( $this->activeCampaign );
+		$this->view->set( 'records', $records );
+		$this->render( 'admin/campaign/reviewers.html' );
+	}
+
+}
+
