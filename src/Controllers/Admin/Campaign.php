be73c479362524f8555ebe5d433aaadb423c8bea
Niharika <niharikakohli29@gmail.com>
2015-05-17 17:00:27 +0000

    [WIP] Allow admin to specify twig template for wikitext export report
    Bug: T93248
    Change-Id: I154ae4d1eff4db2831ac424604468e2cc77bdd99

--- src/Controllers/Admin/Campaign.php
+++ src/Controllers/Admin/Campaign.php
@@ -25,6 +25,7 @@ namespace Wikimedia\IEGReview\Controllers\Admin;
 
 use Wikimedia\IEGReview\Controller;
 use Wikimedia\IEGReview\Arrays;
+use \Twig_Environment;
 
 /**
  * Add a new campaign.
@@ -41,6 +42,7 @@ class Campaign extends Controller {
 				'name' => '',
 				'start_date' => date( 'Y-m-d H:i:s' ),
 				'end_date' => date( 'Y-m-d H:i:s', strtotime( '+30 days' ) ),
+				'wikitext' => file_get_contents( "defaultWikitextTemplate.txt", true )
 			);
 			$questions = array();
 			for ( $idx = 0; $idx < 5; $idx ++ ) {
@@ -92,11 +94,24 @@ class Campaign extends Controller {
 		$this->form->requireStringArray( 'qfooters' );
 		$this->form->requireStringArray( 'qreporthead' );
 
+		$this->form->expectString( 'wikitext',
+			array( 'validate' => function ( $value ) {
+				$twig = new Twig_Environment();
+				try {
+					$twig->parse( $twig->tokenize( $value ) );
+				} catch( \Twig_Error_Syntax $e ) {
+					return false;
+				}
+				return true;
+			} )
+		);
+
 		if ( $this->form->validate() ) {
 			$params = array(
 				'name' => $this->form->get( 'name' ),
 				'start_date' => $this->form->get( 'start_date' ),
 				'end_date' => $this->form->get( 'end_date' ),
+				'wikitext' => $this->form->get( 'wikitext' )
 			);
 
 			$questions = $this->form->get( 'questions' );
@@ -215,7 +230,14 @@ class Campaign extends Controller {
 					);
 				}
 			}
+			$campaignDefaults = array(
+				'name' => $this->form->get( 'name' ),
+				'start_date' => $this->form->get( 'start_date' ),
+				'end_date' => $this->form->get( 'end_date' ),
+				'wikitext' => $this->request->post( 'wikitext' )
+			);
 			$this->flash( 'form_defaults', $quesDefaults );
+			$this->flash( 'campaign', $campaignDefaults );
 		}
 
 		$this->redirect( $this->urlFor( 'admin_campaign', array( 'id' => $id ) ) );
