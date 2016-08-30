26e3cd18e5b7ae53397debe2d19fd6a5745ab60b
Bryan Davis <bd808@wikimedia.org>
2016-08-30 14:59:09 -0600

    Convert ParsoidClient to RESTBase api
    Maniphest Tasks: T114186
    
    Summary:
    Call the RESTBase wikitext-to-html endpoint rather than directly
    interacting with Parsoid.
    
    Bug: T114186
    
    Test Plan:
      * Apply patch
      * Clear out any /var/cache/iegreview content in your test VM
      * Make sure that you aren't setting PARSOID_URL in a local .env file
      * View a campaign proposal
      * See nicely rendered HTML rather than an error message from Parsoid/Varnish
    
    Reviewers: Niharika
    
    Reviewed By: Niharika
    
    Subscribers: jenkins
    
    Differential Revision: https://phabricator.wikimedia.org/D330

--- src/App.php
+++ src/App.php
@@ -67,7 +67,7 @@ class App {
 			'db.user' => Config::getStr( 'DB_USER' ),
 			'db.pass' => Config::getStr( 'DB_PASS' ),
 			'parsoid.url' => Config::getStr( 'PARSOID_URL',
-				'http://parsoid.svc.eqiad.wmnet:8000/enwiki/'
+				'https://en.wikipedia.org/api/rest_v1/transform/wikitext/to/html'
 			),
 			'parsoid.cache' => Config::getStr( 'CACHE_DIR',
 				"{$this->deployDir}/data/cache"
