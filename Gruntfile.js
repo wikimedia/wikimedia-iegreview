9b6d3b52d8a756bebaea09e2446cc945d32d974b
Neha Jha <nj4710@gmail.com>
2018-03-04 20:57:28 -0700

    Added banana checker and missing documentation for i18n messages
    Summary: added banana checker plugin and missing documentation for i18n messages
    
    Reviewers: Niharika, bd808
    
    Reviewed By: bd808
    
    Maniphest Tasks: T184947
    
    Differential Revision: https://phabricator.wikimedia.org/D947

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+/* jshint node:true */
+module.exports = function ( grunt ) {
+
+	MessagesDirs = {
+		"wikimedia/iegreview": "data/i18n"
+	};
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		banana: MessagesDirs,
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: '.',
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jsonlint', 'jshint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
